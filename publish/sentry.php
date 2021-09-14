eef22e3d9bbd268ac8c388076c7f84a9b0885c89
霸气千秋 <zhangshaomin_1990@126.com>
2021-09-14 16:58:06 +0800

    清理 config 文件

--- publish/sentry.php
+++ publish/sentry.php
@@ -9,33 +9,6 @@ return [
 
     'environment' => env('APP_ENV'),
 
-    'breadcrumbs' => [
-        'logs' => true,
-
-        // Capture SQL queries in breadcrumbs
-        'sql_queries' => true,
-
-        // Capture bindings on SQL queries logged in breadcrumbs
-        'sql_bindings' => true,
-
-        // Capture command information in breadcrumbs
-        'command_info' => true,
-    ],
-
-    'tracing' => [
-        // Capture SQL queries as spans
-        'sql_queries' => true,
-
-        // Try to find out where the SQL query originated from and add it to the query spans
-        'sql_origin' => true,
-
-        // Capture views as spans
-        'views' => true,
-
-        // Indicates if the tracing integrations supplied by Sentry should be loaded
-        'default_integrations' => true,
-    ],
-
     // @see: https://docs.sentry.io/platforms/php/configuration/options/#send-default-pii
     'send_default_pii' => false,
 ];
